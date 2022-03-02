b5ce055ce007a164034cae6db6251fe5dcf69f52
Vova <sanik133@gmail.com>
2022-03-02 15:45:40 +0200

    Listen on all addresses

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ fastify.get('/', async (request, reply) => {
 
 const start = async () => {
     try {
-	await fastify.listen(PORT);
+	await fastify.listen(PORT, '0.0.0.0');
     } catch (err) {
 	fastify.log.error(err);
 	process.exit(1);
